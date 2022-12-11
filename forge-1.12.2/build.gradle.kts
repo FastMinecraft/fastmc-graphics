2ec313b081f8ebad0e2ce14b9d8122ed3c6999a2
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-11 05:13:01 -0500

    Added missing mixin configs to forge 1.12.2

--- forge-1.12.2/build.gradle.kts
+++ forge-1.12.2/build.gradle.kts
@@ -2,6 +2,7 @@ import org.spongepowered.asm.gradle.plugins.MixinExtension
 
 forgeProject {
     accessTransformer = "fastmc_at.cfg"
+    mixinConfig("mixins.fastmc-accessor.json", "mixins.fastmc-core.json", "mixins.fastmc-patch.json")
     coreModClass.set("me.luna.fastmc.FastMcGraphicsCoremod")
     devCoreModClass.set("me.luna.fastmc.FastMcGraphicsDevFixCoremod")
 }
