176e45c2e29ce7331914cf107cd63465cc6abdc4
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-12 01:53:03 -0500

    Reverted some removed necessary mixin

--- architectury-1.16.5/common/src/main/kotlin/me/luna/fastmc/mixin/IPatchedVoxelShape.kt
+++ architectury-1.16.5/common/src/main/kotlin/me/luna/fastmc/mixin/IPatchedVoxelShape.kt
@@ -1,28 +1,7 @@
 package me.luna.fastmc.mixin
 
-import net.minecraft.util.shape.FractionalDoubleList
-
 interface IPatchedVoxelShape {
     fun hash(): Int {
         return this.hashCode()
-    }
-
-    companion object {
-        private val cache = arrayOfNulls<FractionalDoubleList>(1024)
-
-        @JvmStatic
-        fun getFractionalDoubleList(sectionCount: Int): FractionalDoubleList {
-            if (sectionCount >= 1024) {
-                return FractionalDoubleList(sectionCount)
-            }
-
-            var value = cache[sectionCount]
-            if (value == null) {
-                value = FractionalDoubleList(sectionCount)
-                cache[sectionCount] = value
-            }
-
-            return value
-        }
     }
 }
