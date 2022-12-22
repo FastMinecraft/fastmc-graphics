df87635babb4adb3d81c4617751db4fcdeb6a79e
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-22 05:46:16 -0500

    Updated to mod-setup 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,12 +15,7 @@ pluginManagement {
 //includeBuild("../mod-setup")
 
 include("shared")
-include("shared:java8")
 include("forge-1.12.2")
 include("architectury-1.16.5", "architectury-1.16.5:common", "architectury-1.16.5:fabric", "architectury-1.16.5:forge")
-
-//include("shared:java16")
-
-include("shared:java17")
 include("architectury-1.18.2", "architectury-1.18.2:common", "architectury-1.18.2:fabric", "architectury-1.18.2:forge")
 include("architectury-1.19.2", "architectury-1.19.2:common", "architectury-1.19.2:fabric", "architectury-1.19.2:forge")
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,11 +1,8 @@
 dependencies {
-    compileOnly("dev.fastmc:fastmc-common:1.0-SNAPSHOT")
-}
-
-subprojects {
-    dependencies {
-        "modCore"("dev.fastmc:fastmc-common:1.0-SNAPSHOT") {
-            isTransitive = false
-        }
+    java8ModCore("dev.fastmc:fastmc-common:1.0-SNAPSHOT:java8") {
+        isTransitive = false
+    }
+    java17ModCore("dev.fastmc:fastmc-common:1.0-SNAPSHOT:java17") {
+        isTransitive = false
     }
 }
