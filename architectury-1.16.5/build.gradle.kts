aa01d393094e0e65c4b117c6e0fc30cd115abcd3
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-27 01:01:15 -0500

    Removed troll at patch

--- architectury-1.16.5/build.gradle.kts
+++ architectury-1.16.5/build.gradle.kts
@@ -5,10 +5,4 @@ architecturyProject {
         "mixins.fastmc-graphics-patch.json"
     )
     accessWidenerPath.set(file("common/src/main/resources/fastmc-graphics.accesswidener").absoluteFile)
-    forge {
-        atPatch {
-            patch("net/minecraft/util/math/Matrix3f", "net/minecraft/util/math/vector/Matrix3f")
-            patch("net/minecraft/util/math/Matrix4f", "net/minecraft/util/math/vector/Matrix4f")
-        }
-    }
 }
