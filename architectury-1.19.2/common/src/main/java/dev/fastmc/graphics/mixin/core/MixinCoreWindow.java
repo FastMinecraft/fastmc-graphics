ae9789da2d8dd01ee5494bacc9f50da913f45a13
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-07-03 00:35:13 -0400

    Fixed mixin mapping issue

--- architectury-1.19.2/common/src/main/java/dev/fastmc/graphics/mixin/core/MixinCoreWindow.java
+++ architectury-1.19.2/common/src/main/java/dev/fastmc/graphics/mixin/core/MixinCoreWindow.java
@@ -3,12 +3,13 @@ package dev.fastmc.graphics.mixin.core;
 import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @SuppressWarnings("ConstantConditions")
 @Mixin(Window.class)
 public abstract class MixinCoreWindow {
-    @Redirect(method = "<init>", at = @org.spongepowered.asm.mixin.injection.At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"))
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J", remap = false))
     private long Redirect$glfwCreateWindow$INVOKE$init(int width, int height, CharSequence title, long monitor, long share) {
         GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 4);
         GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 6);
