7232f471cccb6c3368b018bb6ae9aeff3b2efdad
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-07-05 03:38:32 -0400

    Fixed GuiInventory in-compact glitch

--- forge-1.12.2/src/main/java/dev/fastmc/graphics/mixin/patch/gui/MixinGuiInventory.java
+++ forge-1.12.2/src/main/java/dev/fastmc/graphics/mixin/patch/gui/MixinGuiInventory.java
@@ -5,6 +5,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.entity.EntityLivingBase;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -12,9 +13,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiInventory.class)
 public class MixinGuiInventory {
-    private static float prevRotationYaw = 0.0f;
-    private static float prevRotationPitch = 0.0f;
-    private static float prevRenderYawOffset = 0.0f;
+    @Unique
+    private static float fastmc_graphics$prevRotationYaw = 0.0f;
+    @Unique
+    private static float fastmc_graphics$prevRotationPitch = 0.0f;
+    @Unique
+    private static float fastmc_graphics$prevRenderYawOffset = 0.0f;
+
+    @Inject(method = "drawEntityOnScreen", at = @At("HEAD"))
+    private static void Inject$drawEntityOnScreen$HEAD(
+        int posX,
+        int posY,
+        int scale,
+        float mouseX,
+        float mouseY,
+        EntityLivingBase entity,
+        CallbackInfo ci
+    ) {
+        fastmc_graphics$prevRotationYaw = entity.prevRotationYaw;
+        fastmc_graphics$prevRotationPitch = entity.prevRotationPitch;
+        fastmc_graphics$prevRenderYawOffset = entity.prevRenderYawOffset;
+    }
 
     @Inject(method = "drawEntityOnScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderManager;renderEntity(Lnet/minecraft/entity/Entity;DDDFFZ)V", shift = At.Shift.BEFORE))
     private static void drawEntityOnScreenInvokeRenderEntityPre(
@@ -26,10 +45,6 @@ public class MixinGuiInventory {
         EntityLivingBase entity,
         CallbackInfo ci
     ) {
-        prevRotationYaw = entity.prevRotationYaw;
-        prevRotationPitch = entity.prevRotationPitch;
-        prevRenderYawOffset = entity.prevRenderYawOffset;
-
         entity.prevRotationYaw = entity.rotationYaw;
         entity.prevRotationPitch = entity.rotationPitch;
         entity.prevRenderYawOffset = entity.renderYawOffset;
@@ -40,8 +55,8 @@ public class MixinGuiInventory {
         return ExtensionsKt.getPartialTicks(Minecraft.getMinecraft());
     }
 
-    @Inject(method = "drawEntityOnScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderManager;renderEntity(Lnet/minecraft/entity/Entity;DDDFFZ)V", shift = At.Shift.AFTER))
-    private static void renderEntityPost(
+    @Inject(method = "drawEntityOnScreen", at = @At("RETURN"))
+    private static void Inject$drawEntityOnScreen$RETURN(
         int posX,
         int posY,
         int scale,
@@ -50,8 +65,8 @@ public class MixinGuiInventory {
         EntityLivingBase entity,
         CallbackInfo ci
     ) {
-        entity.prevRotationYaw = prevRotationYaw;
-        entity.prevRotationPitch = prevRotationPitch;
-        entity.prevRenderYawOffset = prevRenderYawOffset;
+        entity.prevRotationYaw = fastmc_graphics$prevRotationYaw;
+        entity.prevRotationPitch = fastmc_graphics$prevRotationPitch;
+        entity.prevRenderYawOffset = fastmc_graphics$prevRenderYawOffset;
     }
 }
